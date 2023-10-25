37c1d033a317fc18f9ae716cf34cf25e0cbb37f5
Meet Mistry <124729207+meetm2003@users.noreply.github.com>
2023-10-25 23:56:56 +0530

    Update editdis.cpp

--- editdis.cpp
+++ editdis.cpp
@@ -15,51 +15,46 @@
 // rorse -> rose (remove 'r')
 // rose -> ros (remove 'e')
 
+// brute force approach 
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 class EditDistance {
 public:
-    // Driver function
-    int editDistance(string word1, string word2) {
-        if (word1.empty()) 
-            return word2.length();
-        if (word2.empty()) 
-            return word1.length();
-        return match(word1, word2, 0, 0);
-    }
-
-    // Helper function
-    int match(string s1, string s2, int i, int j) {
-        // If one of the string's pointer has reached the end of it
-        if (i == s1.length())
-            return s2.length() - j;
-        if (j == s2.length())
-            return s1.length() - i;
-
-        int result;
-
-        // If the current position is the same.
-        if (s1[i] == s2[j]) {
-            result = match(s1, s2, i + 1, j + 1);
-        } else {
-            // Case1: insert
-            int insert = match(s1, s2, i, j + 1);
-            // Case2: delete
-            int remove = match(s1, s2, i + 1, j);
-            // Case3: substitute
-            int substitute = match(s1, s2, i + 1, j + 1);
-
-            result = min(insert, min(remove, substitute)) + 1;
+    int editDistance(string word1, string word2, int m, int n) {
+        int dp[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                // if string is empty...
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+                else if(j == 0){
+                    dp[i][j] = i;
+                }
+                // if char are same of both string... 
+                else if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                // if we need to do operation...
+                else{
+                    dp[i][j] = 1 + min(dp[i][j - 1], // insert
+                                       min(dp[i - 1][j], // remove
+                                       dp[i - 1][j - 1])); // replace
+                }
+            }
         }
-        return result;
+        return dp[m][n];
     }
 };
 
 int main() {
     EditDistance ed;
-    int n = ed.editDistance("meet", "minee");
+    string s1 = "horse";
+    string s2 = "ros";
+    int n = ed.editDistance(s1, s2, s1.length(),s2.length());
     cout << n << endl;
     return 0;
 }
