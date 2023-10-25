94d2bfbe7b13be64d2bce07cb5138e6511c3bbf4
Meet Mistry <124729207+meetm2003@users.noreply.github.com>
2023-10-25 23:57:50 +0530

    Update uniquepath.cpp

--- uniquepath.cpp
+++ uniquepath.cpp
@@ -18,14 +18,19 @@ using namespace std;
 class uniquepath{
    public:
     int up(int m, int n) {
-        std::vector<int> dp(n);
-        dp[0] = 1; // starting point
+        int dp[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[j] += dp[j - 1];
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
             }
         }
-        return dp[n - 1];
+        return dp[m-1][n-1];
     }
 };
 int main(){
@@ -35,4 +40,4 @@ int main(){
 }
 
 // time complexity :: O(mn)
-// space complexity :: O(n)
+// space complexity :: O(m)
